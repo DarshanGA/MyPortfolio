9511af0f27612d8b6fd09a1997750656f1ab5617
DarshanGA <darshanjadav2016@gmail.com>
2025-08-10 10:33:48 +0530

    Initial site skeliton

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import data from "../data/content.json"
+import { ExperienceTimelineComponent } from "./experience-timeline/experience-timeline.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'my-portfolio';
+  fullName = data.fullName;
+  jobTitle = data.jobTitle;
+  aboutMe = data.about;
+  sectionTitles = data.sectionTitles;
 }
