6b3abebcdedcd0b2bdfdfa454d5d1de0f4bdad0c
JaRD <57281300+Its-JaRD@users.noreply.github.com>
2025-08-15 22:04:42 +0530

    added more styling updates to experience timeline comp, added more details model for work experience. (#3)

---
+++ src/app/work-details-model/work-details-model.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-work-details-model',
+  imports: [],
+  templateUrl: './work-details-model.component.html',
+  styleUrl: './work-details-model.component.scss'
+})
+export class WorkDetailsModelComponent {
+
+  @Input({required: true}) companyName = "Placeholder Company Name";
+  @Input({required: true}) keyResponsibilitie!: string[];
+  @Input({required: true}) keyContributions!: string[];
+  @Input({required: true}) detailsId!: number;
+}
